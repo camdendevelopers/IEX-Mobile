ee01cfeb0b6579b684a49e378d0feae38ef891c6
Marcos Ortiz <marcosortiz13@gmail.com>
2019-04-25 15:47:39 -0500

    Initial Commit

---
+++ IEX Mobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IEX Mobile
+//
+//  Created by Marcos Ortiz on 4/25/19.
+//  Copyright © 2019 Camden Developers. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IEX MobileTests/IEX_MobileTests.swift
@@ -0,0 +1,34 @@
+//
+//  IEX_MobileTests.swift
+//  IEX MobileTests
+//
+//  Created by Marcos Ortiz on 4/25/19.
+//  Copyright © 2019 Camden Developers. All rights reserved.
+//
+
+import XCTest
+@testable import IEX_Mobile
+
+class IEX_MobileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
