ac695ad645dce143e666e117fd802fc9f0ad8afe
Marcos Ortiz <marcosortiz13@gmail.com>
2019-04-25 19:46:27 -0500

    Removed Pods reference

--- Pods/Target Support Files/Pods-IEX Mobile/Pods-IEX Mobile-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IEX_Mobile : NSObject
-@end
-@implementation PodsDummy_Pods_IEX_Mobile
-@end
--- Pods/Target Support Files/Pods-IEX MobileUITests/Pods-IEX MobileUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IEX_MobileUITests : NSObject
-@end
-@implementation PodsDummy_Pods_IEX_MobileUITests
-@end
